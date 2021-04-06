f303964e73eb13089bab65dd880d10ca9547caec
248369 <65579295+248369@users.noreply.github.com>
2021-04-06 13:14:43 +0200

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-let msg = "david";
-                let speech = new SpeechSynthesisUtterance();
-                speech.lang = "en";
-                speech.text = msg;
-                speech.volume = 1;
-                speech.rate = 1;
-                speech.pitch = 1;
-                speechSynthesis.cancel();
-                window.speechSynthesis.speak(speech);
